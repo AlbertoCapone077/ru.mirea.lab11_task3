2de240085af579e4f8b446d78bde4a4d82f9e3a8
AlbertoCapone077 <70647789+AlbertoCapone077@users.noreply.github.com>
2020-11-13 00:23:01 +0300

    Add files via upload
    More

---
+++ Divide.java
@@ -0,0 +1,17 @@
+package operation;
+
+import expression.TripleExpression;
+
+public class Divide extends Operation {
+    public Divide(TripleExpression left, TripleExpression right) {
+        super(left, right);
+    }
+
+    protected int evaluate(int left, int right) {
+        return left / right;
+    }
+
+    protected double evaluate(double left, double right) {
+        return left / right;
+    }
+}
